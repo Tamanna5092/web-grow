a6b7cf2e8a1960d35c9821b6c2cceffa8b9ee68b
Tamanna5092 <tamannaaktermoni51@gmail.com>
2026-04-09 23:24:46 +0600

    testimonial

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,8 @@ import {
   FaCode,
   FaDollarSign,
   FaRegFaceSmile,
+  FaRegStar,
+  FaStar,
 } from "react-icons/fa6";
 import {
   MdOutlineCampaign,
@@ -14,6 +16,11 @@ import {
 } from "react-icons/md";
 import { BsLayoutTextWindowReverse } from "react-icons/bs";
 import { TfiLayoutMediaCenterAlt } from "react-icons/tfi";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function About() {
   return (
@@ -49,7 +56,9 @@ export default function About() {
         <p className="text-center lora text-lg text-(--primary-color) mb-2">
           Our Services
         </p>
-        <h2 className="text-3xl md:text-5xl font-extrabold text-center">What We Offer</h2>
+        <h2 className="text-3xl md:text-5xl font-extrabold text-center">
+          What We Offer
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 my-10">
           <div className="flex flex-col group hover:bg-(--primary-color)/10 transition duration-300 p-6 border-2 border-(--primary-color)/50 shadow-lg rounded-lg box-shadow-custom">
             <span className="">
@@ -187,6 +196,170 @@ export default function About() {
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          <h2 className="text-3xl md:text-5xl font-extrabold text-center mt-10 mb-6">
+            Client Testimonials
+          </h2>
+          <p className="text-center lora text-lg text-(--primary-color) mb-2">
+            Hear from our satisfied clients about their experience working with
+            us.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 my-10">
+            <div className="bg-white p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10">
+              <p className="lora text-lg">
+                "WebGrow transformed our online presence with a stunning website
+                and effective digital marketing strategies. Our traffic and
+                sales have significantly increased since working with them."
+              </p>
+              <h4 className="text-xl font-semibold mt-4">John Doe</h4>
+              <p className="text-sm text-gray-500">CEO, Example Company</p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10">
+              <p className="lora text-lg">
+                "The team at WebGrow is incredibly talented and easy to work
+                with. They delivered our project on time and exceeded our
+                expectations. Highly recommend their services!"
+              </p>
+              <h4 className="text-xl font-semibold mt-4">Jane Smith</h4>
+              <p className="text-sm text-gray-500">
+                Marketing Manager, Sample Inc.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="flex flex-col justify-center text-center items-center p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10 cursor-grab">
+                <div>
+                  <img
+                    src="https://i.ibb.co.com/rKydv0Xh/successful-confident-young-businesswoman-ceo-boss-bank-employee-worker-manager-with-arms-crossed-in.jpg"
+                    className="w-20 h-20 border-2 border-(--primary-color) rounded-full object-cover"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-4">
+                  <p className="lora text-lg">
+                    "WebGrow's expertise in web development and digital
+                    marketing has been instrumental in our business growth.
+                    Their team is responsive, creative, and dedicated to
+                    delivering results."
+                  </p>
+                  <p className="flex text-orange-400 gap-1 justify-center my-4">
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaRegStar className="w-5 h-5"></FaRegStar>
+                  </p>
+                  <h4 className="text-xl font-semibold mt-4">Emily Johnson</h4>
+                  <p className="text-sm">Founder, Startup XYZ</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex flex-col justify-center text-center items-center p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10 cursor-grab">
+                <div>
+                  <img
+                    src="https://i.ibb.co.com/kQbYGM9/handsome-bearded-guy-posing-against-white-wall-273609-20597.jpg"
+                    className="w-20 h-20 border-2 border-(--primary-color) rounded-full object-cover"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-4">
+                  <p className="lora text-lg">
+                    "WebGrow transformed our online presence with a stunning
+                    website and effective digital marketing strategies. Our
+                    traffic and sales have significantly increased since working
+                    with them."
+                  </p>
+                  <p className="flex text-orange-400 gap-1 justify-center my-4">
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                  </p>
+                  <h4 className="text-xl font-semibold mt-4">Michael Brown</h4>
+                  <p className="text-sm">CTO, Tech Solutions</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex flex-col justify-center text-center items-center p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10 cursor-grab">
+                <div>
+                  <img
+                    src="https://i.ibb.co.com/7tCznGSG/Whats-App-Image-2025-09-29-at-13-20-27-601f245b.jpg"
+                    className="w-20 h-20 border-2 border-(--primary-color) rounded-full object-cover"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-4">
+                  <p className="lora text-lg">
+                    "WebGrow's expertise in web development and digital
+                    marketing has been instrumental in our business growth.
+                    Their team is responsive, creative, and dedicated to
+                    delivering results."
+                  </p>
+                  <p className="flex text-orange-400 gap-1 justify-center my-4">
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaRegStar className="w-5 h-5"></FaRegStar>
+                  </p>
+                  <h4 className="text-xl font-semibold mt-4">Sarah Lee</h4>
+                  <p className="text-sm">Marketing Director, Creative Agency</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex flex-col justify-center text-center items-center p-6 rounded-lg shadow-md border-2 border-(--primary-color)/10 cursor-grab">
+                <div>
+                  <img
+                    src="https://i.ibb.co.com/kpFhSxW/Shoriful-Islam-Siam.jpg"
+                    className="w-20 h-20 border-2 border-(--primary-color) rounded-full object-cover"
+                    alt=""
+                  />
+                </div>
+                <div className="mt-4">
+                  <p className="lora text-lg">
+                    "WebGrow's expertise in web development and digital
+                    marketing has been instrumental in our business growth.
+                    Their team is responsive, creative, and dedicated to
+                    delivering results."
+                  </p>
+                  <p className="flex text-orange-400 gap-1 justify-center my-4">
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaStar className="w-5 h-5"></FaStar>
+                    <FaRegStar className="w-5 h-5"></FaRegStar>
+                  </p>
+                  <h4 className="text-xl font-semibold mt-4">David Kim</h4>
+                  <p className="text-sm">CEO, Startup Inc.</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
